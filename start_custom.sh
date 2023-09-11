1a74b600991ccbac3f49f404ed4a0c5e160b1b84
GeekROS <15300555770>
2023-09-12 07:31:51 +0800

    1.0.0

--- start_custom.sh
+++ start_custom.sh
@@ -30,10 +30,18 @@ function start_custom() {
     echo "$(fdisk -l ${UBUNTU_IMAGE_PATH})"
 
     OFFSET1=$(fdisk -l ${UBUNTU_IMAGE_PATH} | grep "${UBUNTU_IMAGE_PATH}1" | tr -s ' ' | cut -d' ' -f2)
+    if [[ ! $OFFSET1 =~ ^[0-9]+$ ]]; then
+        echo "Error: OFFSET1 is not a valid number."
+        exit 1
+    fi
     # shellcheck disable=SC2004
     LOOP_DEV1=$(losetup --find --show --offset $(($OFFSET1 * 512)) ${UBUNTU_IMAGE_PATH})
 
     OFFSET2=$(fdisk -l ${UBUNTU_IMAGE_PATH} | grep "${UBUNTU_IMAGE_PATH}2" | tr -s ' ' | cut -d' ' -f2)
+    if [[ ! $OFFSET2 =~ ^[0-9]+$ ]]; then
+        echo "Error: OFFSET2 is not a valid number."
+        exit 1
+    fi
     # shellcheck disable=SC2004
     LOOP_DEV2=$(losetup --find --show --offset $(($OFFSET2 * 512)) ${UBUNTU_IMAGE_PATH})
 
